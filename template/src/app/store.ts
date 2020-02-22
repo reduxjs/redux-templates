e0c2e8539fd4a53c5f16d1483304bbc24477d10a
Ben <benlorantfy@gmail.com>
2020-02-22 15:13:32 -0500

    Fix feature import

--- template/src/app/store.ts
+++ template/src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counterSlice';
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
