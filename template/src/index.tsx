6fdf30a608f73bf09a80571ec5c8ef8ca3651e5f
Ben <benlorantfy@gmail.com>
2020-02-22 14:50:23 -0500

    Fix incorrect import

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './store';
+import { store } from './store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
