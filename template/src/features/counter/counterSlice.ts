f55bbf92b83d87a5672d32728e4c19ca545f37e6
Mark Erikson <mark@isquaredsoftware.com>
2020-03-10 18:18:20 -0400

    Update type declaration usage and comments

--- template/src/features/counter/counterSlice.ts
+++ template/src/features/counter/counterSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { AppThunk } from '../../app/store';
+import { AppThunk, RootState } from '../../app/store';
 
 interface CounterState {
   value: number;
@@ -23,6 +23,7 @@ export const slice = createSlice({
     decrement: state => {
       state.value -= 1;
     },
+    // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
@@ -43,8 +44,8 @@ export const incrementAsync = (amount: number): AppThunk => dispatch => {
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = (state: { counter: CounterState }) =>
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectCount = (state: RootState) =>
   state.counter.value;
 
 export default slice.reducer;
