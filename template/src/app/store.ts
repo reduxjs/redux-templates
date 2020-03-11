abdbfa935d03e303a31e6963f14c42df55ec2889
Jack Zhao <yzhao@grailbio.com>
2020-03-10 20:33:45 -0700

    Make AppThunk generic with return type

--- template/src/app/store.ts
+++ template/src/app/store.ts
@@ -8,4 +8,9 @@ export const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
