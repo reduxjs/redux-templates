7c3f23128081872d536b3e8ce45dd18cd10e0d57
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2021-02-02 17:43:21 +0100

    add should handle increment test

--- template/src/features/counter/counterSlice.spec.js
+++ template/src/features/counter/counterSlice.spec.js
@@ -4,4 +4,13 @@ describe('counter reducer', () => {
   it('should handle initial state', () => {
     expect(counter(undefined, {})).toEqual({value: 0})
   })
+
+  it('should handle increment', () => {
+    expect(
+      counter({ value: 3 }, {
+        type: 'counter/increment',
+      })
+    ).toEqual({ value: 4 });
+  });
+
 })
