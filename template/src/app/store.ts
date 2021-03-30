c0efe83e653a17c8804006d05539f780473a598a
Matt Sutkowski <msutkowski@gmail.com>
2021-03-30 13:17:23 -0700

    Add hooks file, export AppDispatch type

--- template/src/app/store.ts
+++ template/src/app/store.ts
@@ -7,6 +7,7 @@ export const store = configureStore({
   },
 });
 
+export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
--- template/src/features/counter/Counter.tsx
+++ template/src/features/counter/Counter.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import {
   decrement,
   increment,
@@ -8,10 +7,11 @@ import {
   selectCount,
 } from './counterSlice';
 import styles from './Counter.module.css';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 
 export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
   return (
