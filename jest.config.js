cf0a1aa6cee91d0bcaddbeeee1e1b2b4a3e0e1d1
Mark Erikson <mark@isquaredsoftware.com>
2022-04-24 15:40:29 -0400

    Update template code to match CRA latest
    - Updated template deps
    - Removed service worker
    - Added web vitals reporting
    - Updated README contents

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
       '<rootDir>/__mocks__/fileMock.ts',
--- template/src/features/counter/counterSlice.ts
+++ template/src/features/counter/counterSlice.ts
@@ -72,14 +72,13 @@ export const selectCount = (state: RootState) => state.counter.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd = (amount: number): AppThunk => (
-  dispatch,
-  getState
-) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
-  }
-};
+export const incrementIfOdd =
+  (amount: number): AppThunk =>
+  (dispatch, getState) => {
+    const currentValue = selectCount(getState());
+    if (currentValue % 2 === 1) {
+      dispatch(incrementByAmount(amount));
+    }
+  };
 
 export default counterSlice.reducer;
