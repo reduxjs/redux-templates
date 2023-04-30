4ca7370a7f287a84dd091abb554f2e16c0fa9c96
Nick McCurdy <nick@nickmccurdy.com>
2023-04-30 18:55:17 -0400

    Move configs up

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
-      '<rootDir>/__mocks__/fileMock.ts',
+      '<rootDir>/__mocks__/fileMock.{j,t}s',
   },
-  setupFilesAfterEnv: ['<rootDir>/template/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/template/src/setupTests.{j,t}s'],
 };
