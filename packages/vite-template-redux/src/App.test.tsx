cfeac1f74f8bd1edadf9bf7215a1f7764e9bfa88
Nick McCurdy <nick@nickmccurdy.com>
2023-04-30 13:39:14 -0400

    Migrate Redux code into vite template

--- packages/vite-template-redux/src/App.test.tsx
+++ packages/vite-template-redux/src/App.test.tsx
@@ -1,18 +1,14 @@
-import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
+import { render } from "@testing-library/react"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
 import App from "./App"
 
-describe("Simple working test", () => {
-  it("the title is visible", () => {
-    render(<App />)
-    expect(screen.getByText("Vite + React")).toBeInTheDocument()
-  })
+test("renders learn react link", () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  )
 
-  it("should increment count on click", async () => {
-    render(<App />)
-    const counter = screen.getByRole("button", { name: /count is/i })
-    expect(counter.textContent).toBe("count is 0")
-    await userEvent.click(counter)
-    expect(counter.textContent).toBe("count is 1")
-  })
+  expect(getByText(/learn/i)).toBeInTheDocument()
 })
--- packages/vite-template-redux/src/App.tsx
+++ packages/vite-template-redux/src/App.tsx
@@ -1,33 +1,55 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+import logo from "./logo.svg"
+import { Counter } from "./features/counter/Counter"
 import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev/" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
     </div>
   )
 }
