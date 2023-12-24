b823e0077f93558c9b0d5dc6836e509c606f314c
Arya Emami <aryaemami59@yahoo.com>
2023-12-24 09:56:06 -0600

    Rename  `quotesApi` to `quotesApiSlice`

--- packages/vite-template-redux/src/app/store.ts
+++ packages/vite-template-redux/src/app/store.ts
@@ -1,28 +1,29 @@
-import type { Action, ThunkAction } from '@reduxjs/toolkit'
-import { combineSlices, configureStore } from '@reduxjs/toolkit'
-import { counterSlice } from '../features/counter/counterSlice'
-import { quotesApiSlice } from '../features/quotes/quotesApiSlice'
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { counterSlice } from "../features/counter/counterSlice"
+import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
 const rootReducer = combineSlices(counterSlice, quotesApiSlice)
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-    // Adding the api middleware enables caching, invalidation, polling,
-    // and other useful features of `rtk-query`.
-    middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
-    },
-  })
-}
+export const store = configureStore({
+  reducer: rootReducer,
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+  },
+})
 
-// Infer the return type of `makeStore`
-export type AppStore = ReturnType<typeof makeStore>
+// configure listeners using the provided defaults
+// optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+setupListeners(store.dispatch)
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
