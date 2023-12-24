3fb7f9d116b7dd7f089f254746ee14e3ede75ac3
Arya Emami <aryaemami59@yahoo.com>
2023-12-24 13:12:12 -0600

    Format all files

--- packages/cra-template-redux-typescript/template/src/reportWebVitals.ts
+++ packages/cra-template-redux-typescript/template/src/reportWebVitals.ts
@@ -1,15 +1,15 @@
-import { ReportCallback } from 'web-vitals';
+import { ReportCallback } from "web-vitals"
 
 const reportWebVitals = (onPerfEntry?: ReportCallback) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
-      onCLS(onPerfEntry);
-      onFID(onPerfEntry);
-      onFCP(onPerfEntry);
-      onLCP(onPerfEntry);
-      onTTFB(onPerfEntry);
-    });
+    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry)
+      onFID(onPerfEntry)
+      onFCP(onPerfEntry)
+      onLCP(onPerfEntry)
+      onTTFB(onPerfEntry)
+    })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
