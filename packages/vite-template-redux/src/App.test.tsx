885477831a03b82068adb1a8384bb44367369e33
Arya Emami <aryaemami59@yahoo.com>
2023-12-23 23:20:35 -0600

    Update `App.test.tsx` file

--- packages/vite-template-redux/src/App.test.tsx
+++ packages/vite-template-redux/src/App.test.tsx
@@ -1,14 +1,14 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
 import App from "./App"
+import { store } from "./app/store"
 
 test("renders learn react link", () => {
-  const { getByText } = render(
+  render(
     <Provider store={store}>
       <App />
     </Provider>,
   )
 
-  expect(getByText(/learn/i)).toBeInTheDocument()
+  expect(screen.getByText(/learn/i)).toBeInTheDocument()
 })
