1cd13116505ecd402eba120aaeb9865de802f86e
Arya Emami <aryaemami59@yahoo.com>
2023-12-26 15:13:03 -0600

    Replace type assertion inside of `index.tsx` with an actual check

--- packages/cra-template-redux-typescript/template/src/index.tsx
+++ packages/cra-template-redux-typescript/template/src/index.tsx
@@ -1,21 +1,25 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
 import App from "./App"
-import reportWebVitals from "./reportWebVitals"
+import { store } from "./app/store"
 import "./index.css"
+import reportWebVitals from "./reportWebVitals"
 
-const container = document.getElementById("root") as HTMLDivElement
-const root = createRoot(container)
+const container = document.getElementById("root")
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
+if (container) {
+  const root = createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  )
+} else {
+  throw new Error("no root found!")
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
