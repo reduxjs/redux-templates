63944c815daa11abb5f3d7f2c8bfd3899088c591
hidarikani <19387605+hidarikani@users.noreply.github.com>
2023-12-27 15:33:57 +0200

    Included src folder

---
+++ packages/dock-template-redux/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
