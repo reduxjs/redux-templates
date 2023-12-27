d8a089ae4b093d91286ef8dcfbff4f5cc0c71bc3
Arya Emami <aryaemami59@yahoo.com>
2023-12-26 18:11:58 -0600

    Add `makeStore` to help create a unique store instance in unit tests

--- packages/vite-template-redux/src/app/store.ts
+++ packages/vite-template-redux/src/app/store.ts
@@ -7,23 +7,31 @@ import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
 const rootReducer = combineSlices(counterSlice, quotesApiSlice)
+// Infer the `RootState` type from the root reducer
+export type RootState = ReturnType<typeof rootReducer>
 
-export const store = configureStore({
-  reducer: rootReducer,
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
-  middleware: getDefaultMiddleware => {
-    return getDefaultMiddleware().concat(quotesApiSlice.middleware)
-  },
-})
+export const makeStore = (preloadedState?: Partial<RootState>) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    // Adding the api middleware enables caching, invalidation, polling,
+    // and other useful features of `rtk-query`.
+    middleware: getDefaultMiddleware => {
+      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+    },
+    preloadedState,
+  })
+  // configure listeners using the provided defaults
+  // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+  setupListeners(store.dispatch)
+  return store
+}
 
-// configure listeners using the provided defaults
-// optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
-setupListeners(store.dispatch)
+export const store = makeStore()
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+// Infer the type of `store`
+export type AppStore = typeof store
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = AppStore["dispatch"]
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
   RootState,
