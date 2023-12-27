365e3f5a46813d0f322b4bcec9a814b1b326d38a
Arya Emami <aryaemami59@yahoo.com>
2023-12-26 18:11:10 -0600

    Replace type assertion with a runtime check in `main.tsx`

--- packages/vite-template-redux/src/main.tsx
+++ packages/vite-template-redux/src/main.tsx
@@ -1,17 +1,24 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
 import App from "./App"
+import { store } from "./app/store"
 import "./index.css"
 
-const container = document.getElementById("root") as HTMLDivElement
-const root = createRoot(container)
+const container = document.getElementById("root")
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
