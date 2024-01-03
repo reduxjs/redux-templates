0f5f7780ed0e8d31528f4c612d17ca5de8feca23
Arya Emami <aryaemami59@yahoo.com>
2024-01-03 12:37:09 -0600

    Add "@typescript-eslint/no-restricted-imports" rule to `.eslintrc.json`

--- packages/cra-template-redux-typescript/template/src/app/hooks.ts
+++ packages/cra-template-redux-typescript/template/src/app/hooks.ts
@@ -1,3 +1,9 @@
+// This file serves as a central hub for re-exporting pre-typed Redux hooks.
+// These imports are restricted elsewhere to ensure consistent
+// usage of typed hooks throughout the application.
+// We disable the ESLint rule here because this is the designated place
+// for importing and re-exporting the typed versions of hooks.
+/* eslint-disable @typescript-eslint/no-restricted-imports */
 import type { TypedUseSelectorHook } from "react-redux"
 import { useDispatch, useSelector } from "react-redux"
 import type { AppDispatch, RootState } from "./store"
