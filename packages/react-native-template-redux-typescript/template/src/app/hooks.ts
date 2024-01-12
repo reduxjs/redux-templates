27e8451f58170f4d39a160c3f1f8dc5207a35f0b
Arya Emami <aryaemami59@yahoo.com>
2024-01-12 16:39:39 -0600

    Add `hooks.withTypes()` to `hooks.ts`

--- packages/react-native-template-redux-typescript/template/src/app/hooks.ts
+++ packages/react-native-template-redux-typescript/template/src/app/hooks.ts
@@ -4,15 +4,14 @@
 // We disable the ESLint rule here because this is the designated place
 // for importing and re-exporting the typed versions of hooks.
 /* eslint-disable @typescript-eslint/no-restricted-imports */
-import { useEffect, useRef } from "react"
-import { Animated, useWindowDimensions } from "react-native"
-import type { TypedUseSelectorHook } from "react-redux"
-import { useDispatch, useSelector } from "react-redux"
-import type { AppDispatch, RootState } from "./store"
+import { useEffect, useRef } from "react";
+import { Animated, useWindowDimensions } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
 
 /**
  * Custom React hook for calculating viewport units
