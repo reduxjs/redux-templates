754f3a616de228dd49311a20c99c96e8fc49ffc7
Arya Emami <aryaemami59@yahoo.com>
2024-01-12 18:31:15 -0600

    Add `hooks.withTypes()` to `hooks.ts`

--- packages/expo-template-redux-typescript/src/app/hooks.ts
+++ packages/expo-template-redux-typescript/src/app/hooks.ts
@@ -6,13 +6,12 @@
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import { useEffect, useRef } from "react"
 import { Animated, useWindowDimensions } from "react-native"
-import type { TypedUseSelectorHook } from "react-redux"
 import { useDispatch, useSelector } from "react-redux"
 import type { AppDispatch, RootState } from "./store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
 
 /**
  * Custom React hook for calculating viewport units
