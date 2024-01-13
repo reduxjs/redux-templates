15ede3ff3476b6fc7c6ccb1b7a1ccaf95eb72e7d
Arya Emami <aryaemami59@yahoo.com>
2024-01-13 16:54:16 -0600

    Add types to `template.config.js`

--- packages/react-native-template-redux-typescript/template.config.js
+++ packages/react-native-template-redux-typescript/template.config.js
@@ -1,3 +1,16 @@
+/**
+ * Configuration settings for a template.
+ *
+ * @typedef {Object} TemplateConfig
+ * @property {string} placeholderName A unique identifier or name for the placeholder within the template.
+ * @property {string} templateDir The file path to the directory where the template is located.
+ * @property {string} [postInitScript] The path to a script that should be executed after the template initialization is complete (optional).
+ * @property {string} [titlePlaceholder] A placeholder for the title within the template (optional).
+ */
+
+/**
+ * @type TemplateConfig
+ */
 module.exports = {
   placeholderName: "RNTemplate",
   templateDir: "./template",
