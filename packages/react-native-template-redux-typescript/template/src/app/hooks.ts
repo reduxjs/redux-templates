beb257d3329ed1d84ae77c4197152a409f0b3ee4
Arya Emami <aryaemami59@yahoo.com>
2024-01-13 20:36:03 -0600

    Format all files

--- packages/react-native-template-redux-typescript/template/src/app/hooks.ts
+++ packages/react-native-template-redux-typescript/template/src/app/hooks.ts
@@ -4,10 +4,10 @@
 // We disable the ESLint rule here because this is the designated place
 // for importing and re-exporting the typed versions of hooks.
 /* eslint-disable @typescript-eslint/no-restricted-imports */
-import { useEffect, useRef } from "react";
-import { Animated, useWindowDimensions } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from "./store";
+import { useEffect, useRef } from "react"
+import { Animated, useWindowDimensions } from "react-native"
+import { useDispatch, useSelector } from "react-redux"
+import type { AppDispatch, RootState } from "./store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
