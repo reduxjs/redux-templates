a7f55f93c7e79781981908af1502614d2b1d04dd
Arya Emami <aryaemami59@yahoo.com>
2024-12-31 18:36:39 -0600

    Fix Expo template

---
+++ packages/expo-template-redux-typescript/metro.config.js
@@ -0,0 +1,3 @@
+require("ts-node/register")
+
+module.exports = require("./metro.config.ts")
---
+++ packages/expo-template-redux-typescript/metro.config.ts
@@ -0,0 +1,5 @@
+import { getDefaultConfig } from 'expo/metro-config';
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
