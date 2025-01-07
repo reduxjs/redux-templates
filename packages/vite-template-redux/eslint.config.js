68cc56ed070800af42acedd2db67382026b0c404
Arya Emami <aryaemami59@yahoo.com>
2025-01-07 10:26:43 -0600

    Migrate ESLint config of Vite template to flat config

--- packages/vite-template-redux/eslint.config.js
+++ packages/vite-template-redux/eslint.config.js
@@ -1,35 +1,64 @@
-{
-  "extends": [
-    "eslint:recommended",
-    "react-app",
-    "plugin:react/jsx-runtime",
-    "prettier"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": { "project": true, "tsconfigRootDir": "./" },
-  "plugins": ["@typescript-eslint"],
-  "root": true,
-  "ignorePatterns": ["dist"],
-  "rules": {
-    "@typescript-eslint/consistent-type-imports": [
-      2,
-      { "fixStyle": "separate-type-imports" }
-    ],
-    "@typescript-eslint/no-restricted-imports": [
-      2,
-      {
-        "paths": [
-          {
-            "name": "react-redux",
-            "importNames": ["useSelector", "useStore", "useDispatch"],
-            "message": "Please use pre-typed versions from `src/app/hooks.ts` instead."
-          }
-        ]
-      }
-    ]
+import js from "@eslint/js"
+import vitestPlugin from "@vitest/eslint-plugin"
+import prettierConfig from "eslint-config-prettier"
+import reactPlugin from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import globals from "globals"
+import { config, configs } from "typescript-eslint"
+
+const ESLintConfig = config(
+  { name: "global-ignores", ignores: ["**/*.snap", "dist", "coverage"] },
+  { name: "@eslint/js", ...js.configs.recommended },
+  configs.strictTypeChecked,
+  configs.stylisticTypeChecked,
+  vitestPlugin.configs.recommended,
+  {
+    ...reactPlugin.configs.flat?.["jsx-runtime"],
+    name: "main",
+    linterOptions: { reportUnusedDisableDirectives: 2 },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["./*.?(m|c)js?(x)"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      react: reactPlugin,
+      vitest: vitestPlugin,
+    },
+    settings: { vitest: { typecheck: true } },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/consistent-type-definitions": [2, "type"],
+      "@typescript-eslint/consistent-type-imports": [
+        2,
+        {
+          prefer: "type-imports",
+          fixStyle: "separate-type-imports",
+          disallowTypeAnnotations: true,
+        },
+      ],
+      "@typescript-eslint/no-restricted-imports": [
+        2,
+        {
+          paths: [
+            {
+              name: "react-redux",
+              importNames: ["useSelector", "useStore", "useDispatch"],
+              message:
+                "Please use pre-typed versions from `src/app/hooks.ts` instead.",
+            },
+          ],
+        },
+      ],
+    },
   },
-  "overrides": [
-    { "files": ["*.{c,m,}{t,j}s", "*.{t,j}sx"] },
-    { "files": ["*{test,spec}.{t,j}s?(x)"], "env": { "jest": true } }
-  ]
-}
+  { name: "prettier-config", ...prettierConfig },
+)
+
+export default ESLintConfig
