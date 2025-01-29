e8a291f63e805395c733ef95d6cee00087297e73
Arya Emami <aryaemami59@yahoo.com>
2025-01-29 15:57:06 -0600

    Fix `cra-template-redux-typescript`

--- packages/cra-template-redux-typescript/template/src/App.test.tsx
+++ packages/cra-template-redux-typescript/template/src/App.test.tsx
@@ -1,5 +1,5 @@
 import { act, screen } from "@testing-library/react"
-import App from "./App"
+import { App } from "./App"
 import { renderWithProviders } from "./utils/test-utils"
 
 test("App should have correct initial render", () => {
